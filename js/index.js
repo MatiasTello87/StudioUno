0ee7e8787900a6c7f483bb8203b8c8352505a5ad
Matias Tello <matiasfernandotello@gmail.com>
2026-02-21 21:06:01 -0300

    construcción de nacbar en desktop

--- js/index.js
+++ js/index.js
@@ -20,27 +20,6 @@ document.querySelectorAll('.contain-navbar a').forEach(link => {
     });
 });
 
-// console.log("hola wach");
-// // 1. Seleccionamos los elementos
-// const btnHamb = document.querySelector('#btn-hamb');
-// const btnclose = document.querySelector('#btn-close');
-// const navMenu = document.querySelector('#nav-menu');
-
-// btnHamb.addEventListener('click', ()=>{
-//     navMenu.classList.toggle('active');
-//     console.log("aoa");
-//     if(navMenu.classList.contains('active')){
-//       btnclose.addEventListener('click', ()=>{
-//         navMenu.classList.remove('active');
-//       });
-//     }
-
-//     document.querySelectorAll('.contain-navbar a').forEach(link => {
-//     link.addEventListener('click', () => {
-//     navMenu.classList.remove('active');
-//   });
-// });
-// });
 
 
 
